c46b13581bc0c6d2dbb7bcc4e67240fa931b92df
Johannes vd Berg <bakafish.com@gmail.com>
2023-03-08 18:42:07 +0100

    Add level modding infrastructure

--- apply_mod.bat
+++ apply_mod.bat
@@ -1,4 +1,7 @@
 echo off
 
-copy "bin\Debug\net3.5\NineStars.dll" "C:\Program Files (x86)\Steam\steamapps\common\Phoenotopia Awakening\Mods\NineStars\"
-copy Info.json "C:\Program Files (x86)\Steam\steamapps\common\Phoenotopia Awakening\Mods\NineStars\"
+rem copy "bin\Debug\net3.5\NineStars.dll" "C:\Program Files (x86)\Steam\steamapps\common\Phoenotopia Awakening\Mods\NineStars\"
+rem copy Info.json "C:\Program Files (x86)\Steam\steamapps\common\Phoenotopia Awakening\Mods\NineStars\"
+rem copy assets\StreamingAssets\ModifiedLevels "C:\Program Files (x86)\Steam\steamapps\common\Phoenotopia Awakening\Mods\NineStars\ModifiedLevels\"
+
+powershell -Command "Expand-Archive -Force -Path PhoA-NineStars.zip -DestinationPath ""C:\Program Files (x86)\Steam\steamapps\common\Phoenotopia Awakening\Mods\NineStars\"""
--- zip_build.bat
+++ zip_build.bat
@@ -3,11 +3,22 @@ echo off
 rem Create temporary folder
 set temp_folder=%TEMP%\build_temp
 md %temp_folder%
+md %temp_folder%\ModifiedLevels
 
 rem Copy files to temporary folder
 copy bin\Debug\net3.5\NineStars.dll %temp_folder%
 copy Info.json %temp_folder%
 copy README.md %temp_folder%
+copy assets\StreamingAssets\ModifiedLevels %temp_folder%\ModifiedLevels\
+
+rem Convert to xml 
+ren %temp_folder%\ModifiedLevels\*.tmx *.xml
+
+rem Convert to old file format if newer version of Tiled is used
+setlocal EnableDelayedExpansion
+for %%f in ("%temp_folder%\ModifiedLevels\*.xml") do (
+	powershell -Command "(gc %%f) -replace 'class', 'type' | Out-File -encoding ASCII %%f"
+)
 
 rem Zip folder
 powershell Compress-Archive -Update -Path %temp_folder%\* -DestinationPath PhoA-NineStars.zip
